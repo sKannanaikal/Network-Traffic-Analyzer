c69fdc876c8a9fc8d79188bcca922e816abec8df
sKannanaikal <deenathomas@Seans-iMac.fios-router.home>
2021-06-20 17:48:47 -0400

    started to stub out core functions of the program

---
+++ script.py
@@ -0,0 +1,16 @@
+import optparse
+
+def readPCAP(file):
+	packets = []
+	return packets
+
+
+def locateIP(ip):
+	location = ''
+	return location
+
+def main():
+	command = optparse.OptionParser('usage%prog')
+
+if __name__ == "__main__":
+	main()
