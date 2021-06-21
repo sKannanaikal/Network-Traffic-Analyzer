e09fa2f76d7e1dd9d1f38c205defda8d5ce9d747
sKannanaikal <deenathomas@Seans-iMac.fios-router.home>
2021-06-21 19:01:28 -0400

    finalized script proceedign to testing phase

--- script.py
+++ script.py
@@ -1,10 +1,17 @@
 import geoip2.database
 import optparse
+import pyshark
 
-def readPCAP(file, database):
-	packets = []
-	return packets
-
+def readPCAP(wireshark_capture, database):
+	for packet in wireshark_capture:
+		try:
+			destination_address = packet.ip.dst
+			destination_location = locateIP(destination_address, database)
+			source_address = packet.ip.src
+			source_location = locateIP(source_address, database)
+			print('[+] Packet from {src_addr} located in {src_loc} -> Packet sent to {dest_addr} located in {dest_loc}'.format(src_addr=source_address, src_loc=source_location, dest_addr=destination_address, dest_loc=destination_location))
+		except:
+			print("[-] Failed to process this packet's address")
 
 def locateIP(ip, database):
 	match = database.country(ip)
@@ -13,10 +20,13 @@ def locateIP(ip, database):
 
 def main():
 	with geoip2.database.Reader('database/GeoLite2-Country.mmdb') as database:
+		print('[+] Opening up Database the software will be utilizing the GeoLite2-Country Database')
 		command = optparse.OptionParser('usage%prog -f <capture file>')
 		command.add_option('-f', dest='capture', type='string', help='specify the file that will be parsed')
 		file = command.capture
-		readPCAP(file, database)
-		
+		wireshark_capture = pyshark.FileCapture(file)
+		print('[+] Wireshark capture found will proceed to processing the file')
+		readPCAP(wireshark_capture, database)
+
 if __name__ == "__main__":
 	main()
